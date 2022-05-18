8041f32bfffd8549d39a0e51286de45c9983a54e
Irfan <alpergazel@gmail.com>
2022-05-17 23:43:08 -0400

    second commit

--- src/_SchoolRunner.java
+++ src/_SchoolRunner.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class _SchoolRunner {
 
     public static void main(String[] args) {
@@ -8,14 +11,37 @@ public class _SchoolRunner {
         System.out.println(itStudents);
 
         Student student1 = new Student();
-        student1.setFirstName("Filiz");
-        student1.setLastName("Onat");
+        student1.setFirstName("Mary");
+        student1.setLastName("Jones");
         student1.setDepartmentName(ITDepartment.DEPARTMENT_NAME);
         student1.setInstructor(instructor);
 
-        Student student2 = new Student("Ali","Turkes",102,ITDepartment.DEPARTMENT_NAME,instructor);
+        Student student2 = new Student("Allie","Turtle",102,ITDepartment.DEPARTMENT_NAME,instructor);
+
         Student student3 = new Student();
+        student3.setFirstName("Jane");
+        student3.setLastName("Jacks");
+        student3.setDepartmentName(ITDepartment.DEPARTMENT_NAME);
+        student3.setInstructor(instructor);
+
         Student student4 = new Student();
+        student4.setFirstName("Rob");
+        student4.setLastName("Robber");
+        student4.setDepartmentName(ITDepartment.DEPARTMENT_NAME);
+        student4.setInstructor(instructor);
+
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(student1);
+        studentList.add(student2);
+        studentList.add(student3);
+        studentList.add(student4);
+
+        Instructor instructor1 = new Instructor("Ahmedia", "Bayramioa", "Cucumber", 1002, ITDepartment.DEPARTMENT_NAME);
+
+        instructor1.setStudentList(studentList);
+
+        System.out.println(instructor1.getStudentList());
 
     }
 }
